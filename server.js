03d3ae64fa7ea755ee0b6b261f08d765e02539bf
ZQWhiting <zach.whiting@icloud.com>
2020-07-08 17:04:49 -0600

    added api routes for parties table

--- server.js
+++ server.js
@@ -105,6 +105,56 @@ app.post('/api/candidate', ({ body}, res) => {
     });
 });
 
+// get all parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// get a single party
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// delete a party
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
 // ----------Don't add any code below------------- //
 // default response for any other request(Not Found) Catch all
 app.use((req, res) => {
